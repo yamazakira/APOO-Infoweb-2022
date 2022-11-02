2e5e1177b4f24c13011ea974bd58385235d623d9
yamazakira <ymzk11037@gmail.com>
2022-11-02 18:29:44 -0300

    Consultas e relacionamento com exames

---
+++ 2-CRUDConsultas/ClinicaVet/Servico/Procedimentos/ConsultasServico.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Persistencia.DAL.Consultas;
+using Modelos.Procedimentos;
+
+namespace Servico.Procedimentos
+{
+    public class ConsultasServico
+    {
+        private ConsultasDAL ConsultaDAL = new ConsultasDAL();
+        public IQueryable<Consulta> ObterConsultasClassificadasPorData()
+        {
+            return ConsultaDAL.ObterConsultasClassificadasPorData();
+        }
+
+        public Consulta ObterConsultaPorId(long id)
+        {
+            return ConsultaDAL.ObterConsultaPorId(id);
+        }
+        public void GravarConsulta(Consulta Consulta)
+        {
+            ConsultaDAL.GravarConsulta(Consulta);
+        }
+        public Consulta EliminarConsultaPorId(long id)
+        {
+            Consulta Consulta = ConsultaDAL.ObterConsultaPorId(id);
+            ConsultaDAL.EliminarConsultaPorId(id);
+            return Consulta;
+        }
+    }
+}
